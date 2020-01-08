94905975600ea57be544f57b063ec1924fe77dba
Giovani Aguirre <me@its-g.io>
2020-01-08 14:44:06 -0600

    Stopped at extra credit.

--- practice.js
+++ practice.js
@@ -18,8 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
-
+function first(arr) {
+  return arr[0];
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,8 +34,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
-
+function last(arr) {
+  return arr[arr.length - 1];
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -48,7 +50,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family) {
+  family.forEach(person => {
+    alert(person);
+  });
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +69,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters) {
+  for(let i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,12 +88,9 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
-
-
-
-
+function evenFinder(nums) {
+  return nums.filter((num) => num % 2 === 0 ? num : null);
+}
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
